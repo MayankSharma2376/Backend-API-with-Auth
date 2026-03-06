a84f6b0fe03288b0efee6f654f68fa4ecbebd9ef
Mayank Sharma <ms94577621@gmail.com>
2026-03-06 22:54:59 +0530

    New Data

--- src/index.js
+++ src/index.js
@@ -1,32 +1,58 @@
-import dotenv from "dotenv"
-import connectDB from "./config/database.js";
-import app from "./app.js";
-dotenv.config({
-    path: "./.env"
+// import dotenv from "dotenv"
+// import connectDB from "./config/database.js";
+// import app from "./app.js";
+// dotenv.config({
+//     path: "./.env"
 
+// });
+
+// const startServer = async()=>{
+//     try{
+//         await connectDB();
+
+//         app.on("error", (error)=>{
+//             console.log("Error", error)
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT || 8000, ()=>{
+//             console.log(`Server is running on port ${process.env.PORT}`)
+//         })
+
+//     }catch(error){
+//         console.log("MongoDB connection failed!!", error)
+//         throw error;
+
+//     }
+
+// }
+
+import express from "express";
+import dotenv from "dotenv";
+import app from "./app.js";
+import mongoose from "mongoose";
+dotenv.config({
+  path: "./.env",
 });
 
-const startServer = async()=>{
-    try{
-        await connectDB();
+import connectDB from "./config/database.js";
 
-        app.on("error", (error)=>{
-            console.log("Error", error)
-            throw error
-        })
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.use(express.json());
 
+    app.get("/", (req, res) => {
+      res.send("API is Working");
+    });
 
-        app.listen(process.env.PORT || 8000, ()=>{
-            console.log(`Server is running on port ${process.env.PORT}`)
-        })
-
-    }catch(error){
-        console.log("MongoDB connection failed!!", error)
-        throw error;
-
-
-    }
-    
-}
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  } catch (err) {
+    console.error("Not Connected ", err)
+  }
+};
 
 
+startServer()
